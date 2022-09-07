8dfeb11b970d83e73291d46a75eccd4a9f25d5b6
Saif Ali <saifaligouse@gmail.com>
2022-09-07 17:58:50 +0530

    initial commit

---
+++ assets/script/script.js
@@ -0,0 +1,153 @@
+const hamburger_menu = document.querySelector(".hamburger-menu");
+
+const container = document.querySelector(".container");
+
+hamburger_menu.addEventListener("click", () => {
+  container.classList.toggle("active");
+});
+
+$(".child1").on({
+  click: function () {
+    $(".change").fadeOut("slow", function () {
+      $(".change").attr("src", "assets/images/pic1.png");
+      $(".change").fadeIn("slow");
+    });
+  },
+});
+$(".child2").on({
+  click: function () {
+    $(".change").fadeOut("slow", function () {
+      $(".change").attr("src", "assets/images/pic2.png");
+      $(".change").fadeIn("slow");
+    });
+  },
+});
+$(".child3").on({
+  click: function () {
+    $(".change").fadeOut("slow", function () {
+      $(".change").attr("src", "assets/images/pic1.png");
+      $(".change").fadeIn("slow");
+    });
+  },
+});
+$(".child4").on({
+  click: function () {
+    $(".change").fadeOut("slow", function () {
+      $(".change").attr("src", "assets/images/pic2.png");
+      $(".change").fadeIn("slow");
+    });
+  },
+});
+
+$(".child5").on({
+  click: function () {
+    $(".change").fadeOut("slow", function () {
+      $(".change").attr("src", "pic1.png");
+      $(".change").fadeIn("slow");
+    });
+  },
+});
+$(".child6").on({
+  click: function () {
+    $(".change").fadeOut("slow", function () {
+      $(".change").attr("src", "pic2.png");
+      $(".change").fadeIn("slow");
+    });
+  },
+});
+$(".child7").on({
+  click: function () {
+    $(".change").fadeOut("slow", function () {
+      $(".change").attr("src", "pic1.png");
+      $(".change").fadeIn("slow");
+    });
+  },
+});
+$(".child8").on({
+  click: function () {
+    $(".change").fadeOut("slow", function () {
+      $(".change").attr("src", "pic2.png");
+      $(".change").fadeIn("slow");
+    });
+  },
+});
+
+$(".child9").on({
+  click: function () {
+    $(".change").fadeOut("slow", function () {
+      $(".change").attr("src", "assets/images/pic1.png");
+      $(".change").fadeIn("slow");
+    });
+  },
+});
+$(".child10").on({
+  click: function () {
+    $(".change").fadeOut("slow", function () {
+      $(".change").attr("src", "assets/images/pic2.png");
+      $(".change").fadeIn("slow");
+    });
+  },
+});
+$(".child11").on({
+  click: function () {
+    $(".change").fadeOut("slow", function () {
+      $(".change").attr("src", "assets/images/pic1.png");
+      $(".change").fadeIn("slow");
+    });
+  },
+});
+$(".child12").on({
+  click: function () {
+    $(".change").fadeOut("slow", function () {
+      $(".change").attr("src", "assets/images/pic2.png");
+      $(".change").fadeIn("slow");
+    });
+  },
+});
+
+$(".child13").on({
+  click: function () {
+    $(".change").fadeOut("slow", function () {
+      $(".change").attr("src", "pic1.png");
+      $(".change").fadeIn("slow");
+    });
+  },
+});
+$(".child14").on({
+  click: function () {
+    $(".change").fadeOut("slow", function () {
+      $(".change").attr("src", "pic2.png");
+      $(".change").fadeIn("slow");
+    });
+  },
+});
+$(".child15").on({
+  click: function () {
+    $(".change").fadeOut("slow", function () {
+      $(".change").attr("src", "pic1.png");
+      $(".change").fadeIn("slow");
+    });
+  },
+});
+$(".child16").on({
+  click: function () {
+    $(".change").fadeOut("slow", function () {
+      $(".change").attr("src", "pic2.png");
+      $(".change").fadeIn("slow");
+    });
+  },
+});
+
+
+// ======= Scroll Reveal Animation =======
+const sr = ScrollReveal({ 
+  origin: "top",
+  distance: "80px",
+  duration: 2000,
+  reset: false,
+});
+
+// Scroll For Home
+sr.reveal(".title", { delay: 700, distance: "100px" });
+sr.reveal(".btn", { delay: 200, distance: "100px" });
+
